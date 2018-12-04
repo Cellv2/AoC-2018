2ff26a62696bf3fc69c48e3dc20b495b008c69ea
Cellv2 <rich.a@live.com>
2018-12-04 22:07:17 +0000

    Add day 2 part 1

--- day_2/part_1.js
+++ day_2/part_1.js
@@ -6,4 +6,45 @@ const arr = input
     .replace(/\n/g, "\r")
     .split("\r"); //force it to work with any EOL
 
-console.log(arr);
+let twoCount = 0;
+let threeCount = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    let word = arr[i];
+    let twoCountTemp = 0;
+    let threeCountTemp = 0;
+    let letterCountObj = new Object(); //idea is to add all letters to the array as the key, and a value with the number of times it's appeared
+
+    for (let letter in word) {
+        const curLetter = word[letter];
+        if (letterCountObj.hasOwnProperty(curLetter)) {
+            letterCountObj[curLetter] = letterCountObj[curLetter] + 1;
+        } else {
+            const pair = { [curLetter]: 1 };
+            letterCountObj = { ...letterCountObj, ...pair };
+        }
+    }
+
+    for (key in letterCountObj) {
+        if (letterCountObj[key] === 2) {
+            twoCountTemp = 1;
+        }
+
+        if (letterCountObj[key] === 3) {
+            threeCountTemp = 1;
+        }
+    }
+
+    if (twoCountTemp) {
+        twoCount++;
+    }
+
+    if (threeCountTemp) {
+        threeCount++;
+    }
+}
+
+console.log(twoCount);
+console.log(threeCount);
+console.log("Answer: " + twoCount * threeCount);
+
