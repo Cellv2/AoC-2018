5bc1ad61bd49b1afec292d169ea852673a99c2cd
Cellv2 <rich.a@live.com>
2018-12-04 11:39:02 +0000

    Add input.txt and part_2

---
+++ day_1/part_2.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(__dirname + "/input.txt", "utf-8");
+const arr = input
+    .replace(/\r\n/g, "\r")
+    .replace(/\n/g, "\r")
+    .split("\r"); //force it to work with any EOL
+
+let curFreq = 0;
+let seen = [];
+
+while (true) {
+    for (let i = 0; i < arr.length; i++) {
+        seen.push(curFreq);
+        curFreq += parseInt(arr[i]);
+        if (seen.includes(curFreq)) {
+            console.log(`Duplicate found: ${curFreq}`);
+            return;
+        }
+    }
+}
